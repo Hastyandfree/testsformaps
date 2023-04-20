081a2957b15476c593865bbdc4de6db86c60cb06
Harry <harry.stylianou@hotmail.com>
2023-04-20 12:36:29 +1000

    added login page and register page (register has no functionality as of yet). Login takes user to map marker page. Need to add ability to search for location, ability to see user's position and options for makrers displayed (red and green)

---
+++ tutorials/java/MapWithMarker/app/src/main/java/com/example/mapwithmarker/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.example.mapwithmarker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailField;
+    private EditText passwordField;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+
+        emailField = findViewById(R.id.email_field);
+        passwordField = findViewById(R.id.password_field);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailField.getText().toString();
+                String password = passwordField.getText().toString();
+
+                // Validate user input
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please enter email and password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // TODO: Authenticate user with server or database
+
+                // Launch main activity
+                Intent intent = new Intent(LoginActivity.this, MapsMarkerActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ tutorials/java/MapWithMarker/app/src/main/java/com/example/mapwithmarker/RegisterActivity.java
@@ -0,0 +1,11 @@
+package com.example.mapwithmarker;
+
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText nameField;
+    private EditText emailField;
+}
